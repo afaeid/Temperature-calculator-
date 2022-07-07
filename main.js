f7c6738f872f429d9795d3d42ddbd739edd5c4f8
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-07-07 16:51:33 +0600

    ok

---
+++ main.js
@@ -0,0 +1,25 @@
+var OutputScreen = document.querySelector('.output-screen');
+var ModeDirection = document.querySelector('.modes');
+var MainMode = document.querySelector('.main-mode');
+function input(num){
+  OutputScreen.value+= num
+}
+
+function fh(){
+  OutputScreen.value = ((OutputScreen.value*9)/5)+32;
+  ModeDirection.innerText = 'Result is displaying in Fahrenheit from Celsius';
+}
+
+function cl(){
+  OutputScreen.value = '';
+  ModeDirection.innerText= '';
+}
+
+function cs(){
+  OutputScreen.value = ((OutputScreen.value-32)*5)/9;
+  ModeDirection.innerText = 'Reslut is diplaying in Celsius from Fahrenheit';
+}
+
+function del(){
+  OutputScreen.value = OutputScreen.value.slice(0,-1)
+}
